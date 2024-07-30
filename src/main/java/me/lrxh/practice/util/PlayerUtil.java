e942b1556cfc82079ac8212f00c4506871bfab7d
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-07-30 14:53:12 +0100

    !

--- src/main/java/me/lrxh/practice/util/PlayerUtil.java
+++ src/main/java/me/lrxh/practice/util/PlayerUtil.java
@@ -1,150 +1,235 @@
 package me.lrxh.practice.util;
 
 import lombok.experimental.UtilityClass;
-import me.clip.placeholderapi.PlaceholderAPI;
 import me.lrxh.practice.Practice;
-import me.lrxh.practice.match.Match;
-import me.lrxh.practice.match.impl.BasicFreeForAllMatch;
-import me.lrxh.practice.match.impl.BasicTeamMatch;
-import me.lrxh.practice.match.participant.MatchGamePlayer;
-import me.lrxh.practice.participant.GameParticipant;
-import me.lrxh.practice.profile.Profile;
-import me.lrxh.practice.profile.ProfileState;
-import me.lrxh.practice.queue.QueueProfile;
-import org.bukkit.Bukkit;
+import me.lrxh.practice.profile.SpawnTeleportEvent;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.minecraft.server.v1_8_R3.MinecraftServer;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityStatus;
+import net.minecraft.server.v1_8_R3.PacketPlayOutNamedEntitySpawn;
+import org.bukkit.*;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+import org.github.paperspigot.Title;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @UtilityClass
-public final class PlaceholderUtil {
+public class PlayerUtil {
+    private Field STATUS_PACKET_ID_FIELD;
+    private Field STATUS_PACKET_STATUS_FIELD;
+    private Field SPAWN_PACKET_ID_FIELD;
 
-    public static List<String> format(List<String> lines, Player player) {
-        List<String> formattedLines = new ArrayList<>();
-        Profile profile = Profile.getByUuid(player.getUniqueId());
-        QueueProfile queueProfile = profile.getQueueProfile();
-        for (String line : lines) {
-            line = line.replaceAll("<online>", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
-            line = line.replaceAll("<queued>", String.valueOf(Practice.getInstance().getCache().getPlayers().size()));
-            line = line.replaceAll("<in-match>", String.valueOf(Practice.getInstance().getCache().getMatches().size() * 2));
-            line = line.replaceAll("<player>", player.getName());
-            line = line.replaceAll("<ping>", String.valueOf((BukkitReflection.getPing(player))));
-            line = line.replaceAll("<theme>", CC.translate("&" + profile.getOptions().theme().getColor().getChar()));
+    static {
+        try {
+            STATUS_PACKET_ID_FIELD = PacketPlayOutEntityStatus.class.getDeclaredField("a");
+            STATUS_PACKET_ID_FIELD.setAccessible(true);
 
-            if (line.contains("<silent>") && !profile.isSilent()) {
-                continue;
-            } else {
-                line = line.replaceAll("<silent>", "");
+            STATUS_PACKET_STATUS_FIELD = PacketPlayOutEntityStatus.class.getDeclaredField("b");
+            STATUS_PACKET_STATUS_FIELD.setAccessible(true);
+
+            SPAWN_PACKET_ID_FIELD = PacketPlayOutNamedEntitySpawn.class.getDeclaredField("a");
+            SPAWN_PACKET_ID_FIELD.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setLastAttacker(Player victim, Player attacker) {
+        if (attacker == null) {
+            victim.setMetadata("lastAttacker", new FixedMetadataValue(Practice.getInstance(), null));
+        } else {
+            victim.setMetadata("lastAttacker", new FixedMetadataValue(Practice.getInstance(), attacker.getUniqueId()));
+        }
+    }
+
+    public void setInParty(Player victim, Boolean value) {
+        victim.setMetadata("inParty", new FixedMetadataValue(Practice.getInstance(), value));
+    }
+
+    public Boolean inReplay(Player player) {
+        if (player.hasMetadata("inReplay")) {
+            return player.getMetadata("inReplay").get(0).value().equals(true);
+        }
+        return false;
+    }
+
+
+    public static void setImmune(Player player, int ticks) {
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, ticks, 250));
+    }
+
+    public Player getLastAttacker(Player victim) {
+        if (victim.hasMetadata("lastAttacker")) {
+            return Bukkit.getPlayer((UUID) victim.getMetadata("lastAttacker").get(0).value());
+        } else {
+            return null;
+        }
+    }
+
+    public void sendMessage(Player player, ChatComponentBuilder[]... chatComponentBuilders) {
+        List<BaseComponent[]> components = new ArrayList<>();
+        for (ChatComponentBuilder[] builderArray : chatComponentBuilders) {
+            for (ChatComponentBuilder chatComponentBuilder : builderArray) {
+                components.add(chatComponentBuilder.create());
             }
-            if (line.contains("<follow>") && profile.getFollowing().isEmpty()) {
-                continue;
-            } else {
-                line = line.replaceAll("<follow>", "");
-            }
+        }
+        if (player != null) {
+            BaseComponent[] concatenatedComponents = concatenateComponents(components);
+            player.sendMessage(concatenatedComponents);
+        }
+    }
 
-            if (!profile.getFollowing().isEmpty()) {
-                line = line.replaceAll("<followedPlayer>", Bukkit.getPlayer(profile.getFollowing().get(0)).getName());
-            } else {
-                line = line.replaceAll("<followedPlayer>", "");
-            }
 
-            if (profile.getState() == ProfileState.QUEUEING) {
-                line = line.replaceAll("<kit>", queueProfile.getQueue().getKit().getName());
-                line = line.replaceAll("<type>", queueProfile.getQueue().isRanked() ? "Ranked" : "Unranked");
-                line = line.replaceAll("<time>", TimeUtil.millisToTimer(queueProfile.getPassed()));
-                line = line.replaceAll("<minElo>", String.valueOf(queueProfile.getMinRange()));
-                line = line.replaceAll("<maxElo>", String.valueOf(queueProfile.getMaxRange()));
-            }
+    private BaseComponent[] concatenateComponents(List<BaseComponent[]> components) {
+        int totalLength = 0;
+        for (BaseComponent[] component : components) {
+            totalLength += component.length;
+        }
+        BaseComponent[] concatenatedComponents = new BaseComponent[totalLength];
+        int currentIndex = 0;
+        for (BaseComponent[] component : components) {
+            System.arraycopy(component, 0, concatenatedComponents, currentIndex, component.length);
+            currentIndex += component.length;
+        }
+        return concatenatedComponents;
+    }
 
-            if (profile.getParty() != null) {
-                line = line.replaceAll("<leader>", profile.getParty().getLeader().getName());
-                line = line.replaceAll("<party-size>", String.valueOf(profile.getParty().getListOfPlayers().size()));
-            }
-            Match match = profile.getMatch();
-            if (match != null) {
-                if (match instanceof BasicTeamMatch) {
-                    GameParticipant<MatchGamePlayer> participantA = match.getParticipantA();
-                    GameParticipant<MatchGamePlayer> participantB = match.getParticipantB();
+    public static void sendTitle(Player player, String header, String footer, int duration) {
+        player.sendTitle(new Title(CC.translate(header), CC.translate(footer), 1, duration, 10));
+    }
 
-                    boolean aTeam = match.getParticipantA().containsPlayer(player.getUniqueId());
-                    GameParticipant<MatchGamePlayer> playerTeam = aTeam ? participantA : participantB;
-                    GameParticipant<MatchGamePlayer> opponentTeam = aTeam ? participantB : participantA;
+    public ItemStack getPlayerHead(UUID playerUUID) {
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+        skullMeta.setOwner(Bukkit.getPlayer(playerUUID).getName());
+        head.setItemMeta(skullMeta);
+        return head;
+    }
 
-                    line = line.replaceAll("<opponentsCount>", String.valueOf(opponentTeam.getAliveCount()))
-                            .replaceAll("<opponentsMax>", String.valueOf(opponentTeam.getPlayers().size()))
-                            .replaceAll("<teamCount>", String.valueOf(playerTeam.getAliveCount()))
-                            .replaceAll("<teamMax>", String.valueOf(playerTeam.getPlayers().size()));
-                }
-                if (match instanceof BasicFreeForAllMatch) {
-                    BasicFreeForAllMatch basicFreeForAllMatch = (BasicFreeForAllMatch) match;
-                    line = line.replaceAll("<remaning>", String.valueOf(basicFreeForAllMatch.getRemainingTeams()));
-                }
+    public void reset(Player player) {
 
-                if (match.getOpponent(player.getUniqueId()) != null) {
-                    line = line.replaceAll("<opponent>", match.getOpponent(player.getUniqueId()).getName());
-                    line = line.replaceAll("<duration>", match.getDuration());
-                    line = line.replaceAll("<opponent-ping>", String.valueOf(BukkitReflection.getPing(match.getOpponent(player.getUniqueId()))));
-                    line = line.replaceAll("<your-hits>", String.valueOf(match.getGamePlayer(player).getHits()));
-                    line = line.replaceAll("<their-hits>", String.valueOf(match.getGamePlayer(match.getOpponent(player.getUniqueId())).getHits()));
-                    line = line.replaceAll("<hitDifference>", getDifference(player, false));
-                    line = line.replaceAll("<mmcHitDifference>", getDifference(player, true));
-                    line = line.replaceAll("<combo>", getHitCombo(player, false));
-                    line = line.replaceAll("<mmcCombo>", getHitCombo(player, true));
+        if (!player.hasMetadata("frozen")) {
+            player.setWalkSpeed(0.2F);
+            player.setFlySpeed(0.1F);
+        }
 
-                    if (match.getKit().getGameRules().isBedwars()) {
-                        line = line.replaceAll("<bedA>", match.isBedABroken() ? CC.RED + CC.X : CC.GREEN + CC.CHECKMARK);
-                        line = line.replaceAll("<bedB>", match.isBedBBroken() ? CC.RED + CC.X : CC.GREEN + CC.CHECKMARK);
+        player.setHealth(20.0D);
+        player.setSaturation(20.0F);
+        player.setFallDistance(0.0F);
+        player.setFoodLevel(20);
+        player.setFireTicks(0);
+        player.setMaximumNoDamageTicks(20);
+        player.setExp(0.0F);
+        player.setLevel(0);
+        player.setAllowFlight(false);
+        player.setFlying(false);
+        player.setGameMode(GameMode.SURVIVAL);
+        player.getInventory().setArmorContents(new ItemStack[4]);
+        player.getInventory().setContents(new ItemStack[36]);
+        player.getActivePotionEffects().forEach(effect -> player.removePotionEffect(effect.getType()));
+        player.getInventory().setHeldItemSlot(0);
+        player.updateInventory();
+    }
 
-                        boolean aTeam = match.getParticipantA().containsPlayer(player.getUniqueId());
-                        line = line.replaceAll("<youA>", aTeam ? "" : "&7YOU");
-                        line = line.replaceAll("<youB>", !aTeam ? "" : "&7YOU");
+    public void applyFireballKnockback(Location location, List<Entity> entities) {
+        for (Entity entity : entities) {
+            if (!(entity instanceof Player)) continue;
+            Player player = (Player) entity;
+            if (player.getGameMode() != GameMode.SURVIVAL) continue;
+            double vertical = 2 / 2.0;
+            double reference = 3 / 2.0;
+
+            double magnitude = Math.max(-15.0, Math.min(15.0, -1.0 * reference));
+
+            Vector velocity = location.toVector().subtract(player.getLocation().toVector()).normalize();
+            velocity.multiply(magnitude);
+            velocity.setY(vertical);
+
+            player.setVelocity(velocity);
+        }
+    }
+
+    public void doVelocityChange(Player player) {
+        player.setVelocity(player.getVelocity().add(new Vector(0, 0.25, 0)));
+        player.setAllowFlight(true);
+        player.setFlying(true);
+        player.setVelocity(player.getVelocity().add(new Vector(0, 0.15, 0)));
+        player.setAllowFlight(true);
+        player.setFlying(true);
+    }
+
+    public void denyMovement(Player player) {
+        if (player == null) {
+            return;
+        }
+        player.setFlying(false);
+        player.setWalkSpeed(0.0F);
+        player.setFoodLevel(0);
+        player.setSprinting(false);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 200));
+    }
+
+    public void allowMovement(Player player) {
+        if (player == null) {
+            return;
+        }
+        player.setFlying(false);
+        player.setWalkSpeed(0.2F);
+        player.setFoodLevel(20);
+        player.setSprinting(true);
+        player.removePotionEffect(PotionEffectType.JUMP);
+    }
+
+
+    public void teleportToSpawn(Player player) {
+        Location location = Practice.getInstance().getCache().getSpawn() == null ? Practice.getInstance().getServer().getWorlds().get(0).getSpawnLocation() : Practice.getInstance().getCache().getSpawn();
+
+        SpawnTeleportEvent event = new SpawnTeleportEvent(player, location);
+        event.call();
+
+        if (!event.isCancelled() && event.getLocation() != null) {
+            player.teleport(event.getLocation());
+        }
+    }
+
+    public void animateDeath(Player player) {
+        int entityId = EntityUtils.getFakeEntityId();
+        PacketPlayOutNamedEntitySpawn spawnPacket = new PacketPlayOutNamedEntitySpawn(((CraftPlayer) player).getHandle());
+        PacketPlayOutEntityStatus statusPacket = new PacketPlayOutEntityStatus();
+
+        try {
+            SPAWN_PACKET_ID_FIELD.set(spawnPacket, entityId);
+            STATUS_PACKET_ID_FIELD.set(statusPacket, entityId);
+            STATUS_PACKET_STATUS_FIELD.set(statusPacket, (byte) 3);
+            int radius = MinecraftServer.getServer().getPlayerList().d();
+            Set<Player> sentTo = new HashSet<>();
+            for (Entity entity : player.getNearbyEntities(radius, radius, radius)) {
+                if (entity instanceof Player) {
+                    Player watcher = (Player) entity;
+                    if (!watcher.getUniqueId().equals(player.getUniqueId())) {
+                        ((CraftPlayer) watcher).getHandle().playerConnection.sendPacket(spawnPacket);
+                        ((CraftPlayer) watcher).getHandle().playerConnection.sendPacket(statusPacket);
+                        sentTo.add(watcher);
                     }
                 }
+            }
 
-                if (profile.getState() == ProfileState.SPECTATING) {
-                    line = line.replaceAll("<duration>", match.getDuration());
+            Bukkit.getScheduler().runTaskLater(Practice.getInstance(), () -> {
+                for (Player watcher : sentTo) {
+                    ((CraftPlayer) watcher).getHandle().playerConnection.sendPacket(new PacketPlayOutEntityDestroy(entityId));
                 }
-            }
-
-            if (Practice.getInstance().isPlaceholder()) {
-                formattedLines.add(PlaceholderAPI.setPlaceholders(player, line));
-            } else {
-                formattedLines.add(line);
-            }
-        }
-        return formattedLines;
-    }
-
-    public String getDifference(Player player, boolean isMMCDifference) {
-        Profile profile = Profile.getByUuid(player.getUniqueId());
-        Match match = profile.getMatch();
-        Int playerHits = match.getGamePlayer(player).getHits();
-        Int opponentHits = match.getGamePlayer(match.getOpponent(player.getUniqueId())).getHits()
-        String noAdvantage = isMMCDifference ? "&a(+0)" : "&e(0)"
-
-        if (playerHits - opponentHits > 0) {
-            return CC.translate("&a(+" + (playerHits - opponentHits) + ")");
-        } else if (player - opponent < 0) {
-            return CC.translate("&c(" + (playerHits - opponentHits + ")");
-        } else {
-            return CC.translate(noAdvantage);
-        }
-    }
-
-    public String getHitCombo(Player player, boolean isMMCCombo) {
-        Profile profile = Profile.getByUuid(player.getUniqueId());
-        Match match = profile.getMatch();
-        Int playerCombo = match.getGamePlayer(player).getCombo();
-        Int opponentCombo = match.getGamePlayer(match.getOpponent(player.getUniqueId())).getCombo()
-        String noCombo = isMMC ? "&f1st to 100 wins!" : "&fNo Combo"
-
-        if (playerCombo > 1) {
-            return CC.translate("&a" + playerCombo + " Combo");
-        } else if (opponentCombo > 1) {
-            return CC.translate("&c" + opponentCombo + " Combo");
-        } else if (opponentCombo = 0 && playerCombo = 0) {
-            return CC.translate(isMMCCombo);
+            }, 10L);
+        } catch (IllegalAccessException ignored) {
         }
     }
 }
