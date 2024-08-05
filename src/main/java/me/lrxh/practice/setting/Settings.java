06ca94680a42163b655690bb4baaaa758e34bdb7
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2024-08-05 13:44:52 +0100

    Update Settings.java

--- src/main/java/me/lrxh/practice/setting/Settings.java
+++ src/main/java/me/lrxh/practice/setting/Settings.java
@@ -1,38 +1,48 @@
 package me.lrxh.practice.setting;
 
 import me.lrxh.practice.Practice;
+import me.lrxh.practice.util.config.BasicConfigurationFile;
 import lombok.Getter;
 import org.bukkit.Material;
 
 @Getter
 public enum Settings {
+    private BasicConfigurationFile menuConfig = Practice.getInstance().getMenusConfig();
 
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.SHOW_PLAYERS.ENABLED")) {
-        SHOW_PLAYERS(Practice.getInstance().getMenusConfig().getString("SETTINGS.SHOW_PLAYERS.TITLE"), Material.COMPASS, Practice.getInstance().getMenusConfig().getString("SETTINGS.SHOW_PLAYERS.DESCRIPTION")),
+    if (menuConfig.getBoolean("SETTINGS.SHOW_PLAYERS.ENABLED")) {
+        SHOW_PLAYERS(menuConfig.getString("SETTINGS.SHOW_PLAYERS.TITLE"), Material.COMPASS, menuConfig.getString("SETTINGS.SHOW_PLAYERS.DESCRIPTION")),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.SHOW_SCOREBOARD.ENABLED")) {
-        SHOW_SCOREBOARD(Practice.getInstance().getMenusConfig().getString("SETTINGS.SHOW_SCOREBOARD.TITLE"), Material.EYE_OF_ENDER, Practice.getInstance().getMenusConfig().getString("SETTINGS.SHOW_SCOREBOARD.DESCRIPTION")),
+
+    if (menuConfig.getBoolean("SETTINGS.SHOW_SCOREBOARD.ENABLED")) {
+        SHOW_SCOREBOARD(menuConfig.getString("SETTINGS.SHOW_SCOREBOARD.TITLE"), Material.EYE_OF_ENDER, menuConfig.getString("SETTINGS.SHOW_SCOREBOARD.DESCRIPTION")),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.ALLOW_SPECTATORS.ENABLED")) {
-        ALLOW_SPECTATORS(Practice.getInstance().getMenusConfig().getString("SETTINGS.ALLOW_SPECTATORS.TITLE"), Material.EGG, "Allow players to spectate."),
+
+    if (menuConfig.getBoolean("SETTINGS.ALLOW_SPECTATORS.ENABLED")) {
+        ALLOW_SPECTATORS(menuConfig.getString("SETTINGS.ALLOW_SPECTATORS.TITLE"), Material.EGG, "Allow players to spectate."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.ALLOW_DUELS.ENABLED")) {
-        ALLOW_DUELS(Practice.getInstance().getMenusConfig().getString("SETTINGS.ALLOW_DUELS.TITLE"), Material.DIAMOND_SWORD, "Allow Duel Requests."),
+
+    if (menuConfig.getBoolean("SETTINGS.ALLOW_DUELS.ENABLED")) {
+        ALLOW_DUELS(menuConfig.getString("SETTINGS.ALLOW_DUELS.TITLE"), Material.DIAMOND_SWORD, "Allow Duel Requests."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.KILL_EFFECTS.ENABLED")) {
-        KILL_EFFECTS(Practice.getInstance().getMenusConfig().getString("SETTINGS.KILL_EFFECTS.TITLE"), Material.DIAMOND_AXE, "Select Kill Effect."),
+
+    if (menuConfig.getBoolean("SETTINGS.KILL_EFFECTS.ENABLED")) {
+        KILL_EFFECTS(menuConfig.getString("SETTINGS.KILL_EFFECTS.TITLE"), Material.DIAMOND_AXE, "Select Kill Effect."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.THEME.ENABLED")) {
-        THEME(Practice.getInstance().getMenusConfig().getString("SETTINGS.THEME.TITLE"), Material.BOOK, "Select Color Theme."),
+
+    if (menuConfig.getBoolean("SETTINGS.THEME.ENABLED")) {
+        THEME(menuConfig.getString("SETTINGS.THEME.TITLE"), Material.BOOK, "Select Color Theme."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.PING_RANGE.ENABLED")) {
-        PING_RANGE(Practice.getInstance().getMenusConfig().getString("SETTINGS.PING_RANGE.TITLE"), Material.STICK, "Change Ping Range."),
+
+    if (menuConfig.getBoolean("SETTINGS.PING_RANGE.ENABLED")) {
+        PING_RANGE(menuConfig.getString("SETTINGS.PING_RANGE.TITLE"), Material.STICK, "Change Ping Range."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.TIME_CHANGE.ENABLED")) {
-        TIME_CHANGE(Practice.getInstance().getMenusConfig().getString("SETTINGS.TIME_CHANGE.TITLE"), Material.WATCH, "Change Ping Range."),
+
+    if (menuConfig.getBoolean("SETTINGS.TIME_CHANGE.ENABLED")) {
+        TIME_CHANGE(menuConfig.getString("SETTINGS.TIME_CHANGE.TITLE"), Material.WATCH, "Change Ping Range."),
     }
-    if (Practice.getInstance().getMenusConfig().getBoolean("SETTINGS.MENU_SOUNDS.ENABLED")) {
-        MENU_SOUNDS(Practice.getInstance().getMenusConfig().getString("SETTINGS.MENU_SOUNDS.TITLE"), Material.REDSTONE_COMPARATOR, "Toggle Menu Sounds.");
+
+    if (menuConfig.getBoolean("SETTINGS.MENU_SOUNDS.ENABLED")) {
+        MENU_SOUNDS(menuConfig.getString("SETTINGS.MENU_SOUNDS.TITLE"), Material.REDSTONE_COMPARATOR, "Toggle Menu Sounds.");
     }
 
     private final String name;
